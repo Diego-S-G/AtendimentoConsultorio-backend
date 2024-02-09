b68484ed4bec19f85f5b402680fb3139bf978c1b
Diego-S-G <140908243+Diego-S-G@users.noreply.github.com>
2024-02-09 16:53:01 -0300

    Cria entidade e mapeamento de Médico.

---
+++ AtendimentoConsultorio.Domain/Entities/Medico.cs
@@ -0,0 +1,9 @@
+﻿namespace AtendimentoConsultorio.Domain.Entities
+{
+    public class Medico
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Especialidade { get; set; }
+    }
+}
---
+++ AtendimentoConsultorio.Infrastructure/ConfigurationMap/MedicoEntityConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using AtendimentoConsultorio.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AtendimentoConsultorio.Infrastructure.ConfigurationMap
+{
+    public class MedicoEntityConfiguration : IEntityTypeConfiguration<Medico>
+    {
+        public void Configure(EntityTypeBuilder<Medico> builder)
+        {
+            builder.ToTable(nameof(Medico));
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property((x) => x.Nome)
+                .HasColumnName("Nome")
+                .HasColumnType("varchar")
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property((x) => x.Especialidade)
+                .HasColumnName("Especialidade")
+                .HasColumnType("varchar")
+                .HasMaxLength(20)
+                .IsRequired();
+        }
+    }
+}
